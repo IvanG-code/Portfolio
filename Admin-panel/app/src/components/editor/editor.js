e82554e72a33845343ae41bb6b9debcddb7cda1d
Ivan <igtanchak8@gmail.com>
2020-06-02 23:07:04 +0300

    Gulp

---
+++ Admin-panel/app/src/components/editor/editor.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, {Component} from 'react';
+
+export default class Editor extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageList: [],
+            newPageName: ""
+        }
+    }
+
+    componentDidMount() {
+        this.loadPageList();
+    }
+
+    loadPageList() {
+        axios
+            .get("./api")
+            .then(res => this.setState({pageList: res.data}))
+    }
+
+    render () {
+        const {pageList} = this.state;
+        const pages = pageList.map(page => {
+            return (
+            <h1>{page}</h1>
+            )
+        });
+        return (
+            <>
+                <input type="text"/>
+                <button>Создать страницу</button>
+                {pages}
+            </>
+        )
+    }
+}
